61aeaf2cf3d26675477a4bc459d353d0f7ad5f27
Joseph Davitt <joseph.j.davitt@gmail.com>
2023-07-22 17:21:01 -0700

    add notes

--- test.rb
+++ test.rb
@@ -1,4 +1,20 @@
+# puts adds a line after, print is inline
+
 puts "   /|"
 puts "  / |"
 puts " /  |"
 puts "/___|"
+
+# variables
+
+character_name = "Andrew"
+character_age = "50"
+# concat works for ruby
+puts (character_name + " is " + character_age + " years old.")
+# variables reassigned with same syntax
+character_age = "29"
+# string interpolation works like template literal. Only works with double quotes
+puts ("He wished he was #{character_age}")
+
+# escape characters - \n is 'new line'
+puts "one\ntwo\nthree"
