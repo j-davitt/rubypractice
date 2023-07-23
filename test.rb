67b701717c0dc166679204056a55c72ed4016ef8
Joseph Davitt <joseph.j.davitt@gmail.com>
2023-07-22 17:30:50 -0700

    add notes

--- test.rb
+++ test.rb
@@ -12,9 +12,12 @@ character_age = "50"
 # concat works for ruby
 puts (character_name + " is " + character_age + " years old.")
 # variables reassigned with same syntax
-character_age = "29"
+character_age = 29
 # string interpolation works like template literal. Only works with double quotes
 puts ("He wished he was #{character_age}")
 
-# escape characters - \n is 'new line'
+# escape character \ - \n is 'new line'
 puts "one\ntwo\nthree"
+
+# null = nil
+isnull = nil
